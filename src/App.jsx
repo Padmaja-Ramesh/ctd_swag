9b697b16106adc9304406bd2c86b161d75b84c9c
padmaja-ramesh <padmaja-ramesh@users.noreply.github.com>
2025-07-22 11:37:34 -0400

    Todoform component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import TodoList from "./TodoList";
+import TodoForm from "./TodoForm";
 
 function App() {
   return (
     <div>
       <h1>Hi, welcome to code the dream swag page</h1>
+      <TodoForm></TodoForm>
       <TodoList></TodoList>
     </div>
   );
---
+++ src/TodoForm.jsx
@@ -0,0 +1,13 @@
+function TodoForm() {
+  return (
+    <div>
+      <form>
+        <label for="todoTitle">Todo</label>
+        <input id="todoTitle"></input>
+        <button>Add Todo</button>
+      </form>
+    </div>
+  );
+}
+
+export default TodoForm;

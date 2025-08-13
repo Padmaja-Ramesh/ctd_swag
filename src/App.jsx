1fae0b571e4bc52dc4fe37e0576c7c410abd08d9
padmaja-ramesh <padmaja-ramesh@users.noreply.github.com>
2025-08-13 18:11:47 -0400

    feature directory updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import TodoList from "./TodoList";
-import TodoForm from "./TodoForm";
+import TodoForm from "../src/features/TodoForm";
 import "./App.css";
 
 function App() {

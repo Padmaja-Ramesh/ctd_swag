80eec3f89432a8feaabd89b10c367278b5b24939
padmaja-ramesh <padmaja-ramesh@users.noreply.github.com>
2025-09-29 23:08:11 -0400

    Create Reducer

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -19,6 +19,51 @@ const actions = {
   clearError: "clearError",
 };
 
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case actions.fetchTodos:
+      return {
+        ...state,
+      };
+    case actions.loadTodos:
+      return {
+        ...state,
+      };
+    case actions.completeTodo:
+      return {
+        ...state,
+      };
+    case actions.revertTodo:
+      return {
+        ...state,
+      };
+    case actions.updateTodo:
+      return {
+        ...state,
+      };
+    case actions.addTodo:
+      return {
+        ...state,
+      };
+    case actions.startRequest:
+      return {
+        ...state,
+      };
+    case actions.endRequest:
+      return {
+        ...state,
+      };
+    case actions.setLoadError:
+      return {
+        ...state,
+      };
+    case actions.clearError:
+      return {
+        ...state,
+      };
+  }
+}
+
 const initialState = {
   TodoList: [],
   isLoading: false,
@@ -29,4 +74,4 @@ const initialState = {
   sortField: "createdTime",
 };
 
-export { initialState, actions };
+export { initialState, actions, reducer };
